00a4d95fdab7b5feeb77b05e2a96b632d5e0bbe3
hansdf <dornsback@gmail.com>
2024-05-21 08:54:25 -0300

    update greeting
    takes time from date(), getHours() to use the correct greeting

--- javascript.js
+++ javascript.js
@@ -1,15 +1,20 @@
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }
-  let time = getRandomInt(24);
-  let currentState
-  if (time < 12) {
-      currentState = ("Good morning (●'◡'●)")
-  }
-  else if (time <= 18) {
-      currentState = ("Good afternoon (●'◡'●)")
-  }
-  else {
-      currentState = ("Good evening (●'◡'●)")
-  }
-  document.getElementById("title").innerHTML = currentState
+}
+
+const d = new Date();
+let hour = d.getHours();
+
+let currentState
+if (hour < 12) {
+    currentState = ("Good morning (●'◡'●)")
+} else if (hour <= 18) {
+    currentState = ("Good afternoon (●'◡'●)")
+} else {
+  currentState = ("Good evening (●'◡'●)")
+}
+
+let greeting = document.querySelector("#title")
+greeting.textContent = currentState
+
+console.log("the current hour is: " + hour)
